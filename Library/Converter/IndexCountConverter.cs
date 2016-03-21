5f3aa692e896d77659e13cb994196b241d33cc77
wujunliangCanDao <wujunliang@candaochina.com>
2016-03-21 14:07:07 +0800

    品项销售明细界面部分。

---
+++ Library/Converter/IndexCountConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Library.Converter
+{/// <summary>
+    /// index 转为count 
+    /// </summary>
+    public class IndexCountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value != null && value is int)
+            {
+                Int32 temp = System.Convert.ToInt32(value);
+                return temp < 0 ? string.Format("[{0}]", Math.Abs(temp)) : (temp + 1).ToString();//小于0是分组的序号，进行特殊处理。
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- Library/ReportViewWindow.xaml.cs
+++ Library/ReportViewWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Services.Client;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -12,6 +14,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Common;
+using Models;
+using Models.Enum;
+using WebServiceReference.IService;
+using WebServiceReference.ServiceImpl;
 
 namespace Library
 {
@@ -20,15 +27,71 @@ namespace Library
     /// </summary>
     public partial class ReportViewWindow
     {
+        private DishSaleFullInfo _dishSaleFullInfo;
+
         public ReportViewWindow()
         {
             InitializeComponent();
+            DishSaleInfos = new ObservableCollection<DishSaleInfo>();
+
+            DataContext = this;
         }
+
+        /// <summary>
+        /// 品项销售信息集合。
+        /// </summary>
+        public ObservableCollection<DishSaleInfo> DishSaleInfos { get; private set; }
 
         private void ButtonCancel_OnClick(object sender, RoutedEventArgs e)
         {
             DialogResult = false;
             Close();
+        }
+
+        private void ButtonPeriod_OnClick(object sender, RoutedEventArgs e)
+        {
+            var typeString = ((Button)sender).Tag as string;
+            var enumType = (EnumDishSalePeriodsType)Enum.Parse(typeof(EnumDishSalePeriodsType), typeString);
+
+            TaskService.Start(enumType, GetDishSaleDataProcess, GetDishSaleDataComplete, "获取品项明细数据中...");
+        }
+
+        private object GetDishSaleDataProcess(object param)
+        {
+            IRestaurantService service = new RestaurantServiceImpl();
+            return service.GetDishSaleInfo((EnumDishSalePeriodsType)param);
+        }
+
+        private void GetDishSaleDataComplete(object arg)
+        {
+            var result = (Tuple<string, DishSaleFullInfo>)arg;
+            if (!string.IsNullOrEmpty(result.Item1))
+            {
+                frmBase.Warning(result.Item1);
+                return;
+            }
+
+            _dishSaleFullInfo = result.Item2;
+            DishSaleInfos.Clear();
+            if (result.Item2 != null)
+            {
+                Dispatcher.BeginInvoke((Action) delegate
+                {
+                    try
+                    {
+                        result.Item2.DishSaleInfos.ForEach(DishSaleInfos.Add);
+                    }
+                    catch (Exception ex)
+                    {
+                        AllLog.Instance.E(ex.Message);
+                    }
+                });
+            }
+        }
+
+        private void ReportViewWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ButtonPeriod_OnClick(BtToday, null);//默认获取今天的数据。
         }
     }
 }
