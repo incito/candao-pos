79db04bcdcddf8826532118be0c9f5c480801af5
wujunliangCanDao <wujunliang@candaochina.com>
2016-05-13 02:30:43 +0800

    转测通过，修改版本号V1.6.8。

--- Library/JdeReloadSelectWindow.Designer.cs
+++ Library/JdeReloadSelectWindow.Designer.cs
@@ -45,7 +45,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(105, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(142, 4);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(64, 65);
             this.pictureBox1.TabIndex = 0;
