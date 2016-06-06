09bebf363be82c87ffce6ba0ce33949111b31b17
wujunliangCanDao <wujunliang@candaochina.com>
2016-06-06 10:09:26 +0800

    转测通过，修改版本号为1.7.9。

--- Main/Properties/AssemblyInfo.cs
+++ Main/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.7.8.2")]
-[assembly: AssemblyFileVersion("1.7.8.2")]
+[assembly: AssemblyVersion("1.7.9.0")]
+[assembly: AssemblyFileVersion("1.7.9.0")]
