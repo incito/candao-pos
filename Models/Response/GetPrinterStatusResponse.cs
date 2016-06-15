4ba1422ce62b7521f3a577daf411908e7857826d
wujunliangCanDao <wujunliang@candaochina.com>
2016-06-15 10:14:22 +0800

    提交漏掉的几个文件。

---
+++ Models/Response/GetPrinterStatusResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Models.Response
+{
+    public class GetPrinterStatusResponse
+    {
+        public bool isSuccess { get; set; }
+
+        public string errorMsg { get; set; }
+
+        public List<PrinterStatusInfoResponse> data { get; set; }
+    }
+}
---
+++ Models/Response/PrinterStatusInfoResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Models.Response
+{
+    public class PrinterStatusInfoResponse
+    {
+        public string ip { get; set; }
+
+        public string name { get; set; }
+
+        public int status { get; set; }
+
+        public string statusTitle { get; set; } 
+    }
+}
