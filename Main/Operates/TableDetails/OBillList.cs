018c64c722131a06e75ffaf19476496cf5fb99d0
ChenHuCanDao <chenhu@candaochina.com>
2016-06-15 10:02:49 +0800

    漏提交，补提交

---
+++ Main/Operates/TableDetails/OBillList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KYPOS.Operates.TableDetails
+{
+    /// <summary>
+    /// 菜单操作管理类
+    /// </summary>
+    public class OBillList
+    {
+        #region 字段
+        //当前DataGridView
+        private DataGridView _currentDataGrid = null;
+
+        private DataSet _currentData = null;
+        #endregion
+
+        #region 属性
+        /// <summary>
+        /// 当前数据集
+        /// </summary>
+        private DataSet CurrentData
+        {
+            set
+            {
+                _currentData = value;
+            }
+            get
+            {
+                return _currentData;
+            }
+        }
+        #endregion
+
+        #region 构造函数
+        public OBillList(DataGridView dataGrid)
+        {
+            _currentDataGrid = dataGrid;
+            _currentDataGrid.AutoGenerateColumns = false;
+            _currentDataGrid.Tag = 0;
+        }
+        #endregion
+
+        #region 公共方法
+        /// <summary>
+        /// 设置数据集
+        /// </summary>
+        /// <param name="ds"></param>
+        public void SetData(DataSet ds)
+        {
+            _currentData = ds;
+            _currentDataGrid.DataSource = _currentData;
+        }
+
+        public void ChanageRowValue()
+        {
+
+        }
+        #endregion
+    }
+}
