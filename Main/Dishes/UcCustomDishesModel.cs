5403a170b5a80d880c83091db50f2e2f00c758ad
ChenHuCanDao <chenhu@candaochina.com>
2016-06-20 10:58:55 +0800

    新增菜品定义界面

---
+++ Main/Dishes/UcCustomDishesModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CanDaoCD.Pos.Common.Classes.Mvvms;
+
+namespace KYPOS.Dishes
+{
+   public class UcCustomDishesModel:ViewModelBase
+   {
+       private string _dishesName;
+       private string _price;
+       private string _amount;
+
+       /// <summary>
+       /// 菜名
+       /// </summary>
+       public string DishesName
+       {
+           set
+           {
+               _dishesName = value;
+               RaisePropertyChanged(() => DishesName);
+           }
+            get { return _dishesName; }
+       }
+
+       /// <summary>
+       /// 价格
+       /// </summary>
+       public string Price
+       {
+           set
+           {
+               _price = value;
+               RaisePropertyChanged(() => Price);
+           }
+           get { return _price; }
+       }
+
+       /// <summary>
+       /// 数量
+       /// </summary>
+       public string Amount
+       {
+           set
+           {
+               _amount = value;
+               RaisePropertyChanged(() => Amount);
+           }
+           get { return _amount; }
+       }
+   }
+}
