12aa08d34873158c7b279bb5a20e46944a9e07c2
ChenHuCanDao <chenhu@candaochina.com>
2016-07-12 20:55:15 +0800

    漏上传文件

---
+++ CanDaoCD.Pos.Common/Models/VipModels/MVipCardInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CanDaoCD.Pos.Common.Models.VipModels
+{
+    /// <summary>
+    /// 会员卡信息
+    /// </summary>
+    public class MVipCardInfo
+    {
+        /// <summary>
+        /// 卡号
+        /// </summary>
+        public string CardNum { set; get; }
+        /// <summary>
+        /// 余额
+        /// </summary>
+        public float Balance { set; get; }
+        /// <summary>
+        /// 金额
+        /// </summary>
+        public float Integral { set; get; }
+        /// <summary>
+        /// 优惠券余额
+        /// </summary>
+        public float CouponBalance { set; get; }
+
+        /// <summary>
+        /// 会员交易号
+        /// </summary>
+        public string TraceCode { set; get; }
+
+        /// <summary>
+        /// 卡类型{0:虚拟卡，1：实体卡，2：微会员}
+        /// </summary>
+        public int CardType { set; get; }
+
+        /// <summary>
+        /// 卡等级
+        /// </summary>
+        public int CardLevel { set; get; }
+
+        /// <summary>
+        /// 卡等级名称
+        /// </summary>
+        public string CardLevelName { set; get; }
+
+        /// <summary>
+        /// 卡状态{0 注销 1 正常 2 挂失}
+        /// </summary>
+        public int CardState { set; get; }
+    }
+}
