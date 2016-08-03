fbb91359de18ee990c2f9bfcbfe432f1872df917
ChenHuCanDao <chenhu@candaochina.com>
2016-08-03 10:25:00 +0800

    修改餐台Id格式及新增优惠券接口

---
+++ CanDao.Pos.Model/Request/DelPreferentialRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CanDao.Pos.Model.Request
+{
+   public class DelPreferentialRequest
+    {
+       public string orderid { set; get; }
+       /// <summary>
+       /// 优惠关系ID
+       /// </summary>
+       public string DetalPreferentiald { set; get; }
+       /// <summary>
+       /// 是否全部清除（0：否1：是）默认0
+       /// </summary>
+       public string clear { set; get; }
+    }
+}
