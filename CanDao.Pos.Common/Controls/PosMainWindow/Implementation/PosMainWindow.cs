fe7fc4f8c01c8a12ba48c477242e3add18cf3120
wujunliangCanDao <wujunliang@candaochina.com>
2016-09-02 18:25:32 +0800

    修复#8956主界面添加最小化按钮。

---
+++ CanDao.Pos.Common/Controls/PosMainWindow/Implementation/PosMainWindow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CanDao.Pos.Common.Controls
+{
+    public class PosMainWindow : Window
+    {
+        static PosMainWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(PosMainWindow), new FrameworkPropertyMetadata(typeof(PosMainWindow)));
+        }
+
+        public CornerRadius WindowCornerRadius
+        {
+            get { return (CornerRadius)GetValue(WindowCornerRadiusProperty); }
+            set { SetValue(WindowCornerRadiusProperty, value); }
+        }
+
+        public static readonly DependencyProperty WindowCornerRadiusProperty =
+            DependencyProperty.Register("WindowCornerRadius", typeof(CornerRadius), typeof(PosMainWindow), new PropertyMetadata(new CornerRadius(0)));
+
+        /// <summary>
+        /// 标题中间部分内容控件。
+        /// </summary>
+        public Object TitleContent
+        {
+            get { return GetValue(TitleContentProperty); }
+            set { SetValue(TitleContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleContentProperty =
+            DependencyProperty.Register("TitleContent", typeof(Object), typeof(PosMainWindow), new PropertyMetadata(null));
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            var closeBtn = GetTemplateChild("PART_CloseBtn") as Button;
+            if (closeBtn != null)
+                closeBtn.Click += CloseBtnOnClick;
+
+            var minBtn = GetTemplateChild("PART_Min") as Button;
+            if (minBtn != null)
+                minBtn.Click += MinBtnOnClick;
+
+            var titleCtrl = GetTemplateChild("PART_Title") as Border;
+            if (titleCtrl != null)
+            {
+                titleCtrl.MouseLeftButtonDown += TitleCtrlOnMouseLeftButtonDown;
+                titleCtrl.CornerRadius = new CornerRadius(WindowCornerRadius.TopLeft, WindowCornerRadius.TopRight, 0, 0);
+            }
+        }
+
+        private void MinBtnOnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void TitleCtrlOnMouseLeftButtonDown(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            DragMove();
+        }
+
+        private void CloseBtnOnClick(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Close();
+        }
+
+    }
+}
